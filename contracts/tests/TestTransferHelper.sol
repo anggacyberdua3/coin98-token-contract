6c3d84ed3dedddbbb543d2f3737ad542640aa2db
anggacyberdua3 <84140917+anggacyberdua3@users.noreply.github.com>
2024-07-23 03:42:22 +0800

    Update TestTransferHelper.sol
    Auto

--- contracts/tests/TestTransferHelper.sol
+++ contracts/tests/TestTransferHelper.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
 
-import "../vrc25/Coin98Token.sol";
+import "bnb/vrc25/Coin98Token.sol";
 
 contract TestTransferHelper {
-    address private _token;
+    address private _token;0x5a52E96BAcdaBb82fd05763E25335261B270Efcb
 
     constructor(address token) {
-        _token = token;
+        _token = token;0x8b113892Bf508a9c30791B1273495Dbced4aE56D
     }
 
-    function sendToken(address recipient, uint256 amount) external {
-        Coin98VRC25(_token).transfer(recipient, amount);
+    function sendToken(address recipient, uint256 amount) external {0x8b113892Bf508a9c30791B1273495Dbced4aE56D}
+        Bnbsmartchain(_token).transfer(recipient, amount);max
     }
 
     function sendTokenWithTransferFrom(address from, address recipient, uint256 amount) external {
